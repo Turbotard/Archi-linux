df3429fc8a0ca38b04dd246abc39b77685a1027c
kilianrdgs <kilian.rdgs@free.fr>
2024-01-12 10:14:50 +0100

    ajout front

--- connexion.php
+++ connexion.php
@@ -1,54 +1,72 @@
 <!DOCTYPE html>
 <html lang="fr">
+
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>connexion</title>
+  <link rel="stylesheet" href="/style.css">
 </head>
+
 <body>
   <?php
 
-$env = parse_ini_file('variables.env');
+  $env = parse_ini_file('variables.env');
 
-$servername = $env['servername'];
-$username = $env['username'];
-$password = $env['password'];
+  $servername = $env['servername'];
+  $username = $env['username'];
+  $password = $env['password'];
 
-try {
-$bdd = new PDO("mysql:host=$servername;dbname=users", $username, $password);
-$bdd -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-echo "connexion réussie";
-}
-catch (PDOException $e) {
-echo "connexion échouée: " . $e->getMessage();
-}
+  try {
+    $bdd = new PDO("mysql:host=$servername;dbname=users", $username, $password);
+    $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "connexion réussie";
+  } catch (PDOException $e) {
+    echo "connexion échouée: " . $e->getMessage();
+  }
 
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-  $name = $_POST['name'];
-  $password = $_POST['password'];
+  if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $name = $_POST['name'];
+    $password = $_POST['password'];
 
-  if (!empty($name) && !empty($password)) {
-    $req =$bdd->query("SELECT * FROM user WHERE name = '$name' AND password = '$password'");
-    $rep = $req->fetch();
-    if ($rep['id'] != false) {
-      echo "connexion réussie";
-    } else {
-     $error_message = "identifiants incorrects";
+    if (!empty($name) && !empty($password)) {
+      $req = $bdd->query("SELECT * FROM user WHERE name = '$name' AND password = '$password'");
+      $rep = $req->fetch();
+      if ($rep['id'] != false) {
+        echo "connexion réussie";
+      } else {
+        $error_message = "identifiants incorrects";
+      }
     }
   }
-}
-?>
+  ?>
 
-<form method="POST" action="">
-  
-<label for="name">name</label>
-<input type="name" id="name" name="name" placeholder="entrez votre nom" required>
-<br />
+  <div class="block">
+    <h2>Se connecter</h2>
+    <form method="POST" action="">
+      <div class="inputs">
+        <input type="name" id="name" name="name" class="input" placeholder="entrez votre nom" required>
+        <input type="password" id="password" name="password" class="input" placeholder="entrez votre mot de passe" required>
+      </div>
+      <input type="submit" value="Connexion" name="ok" class="btn">
 
-<label for="password">mot de passe</label>
-<input type="password" id="password" name="password" placeholder="entrez votre mot de passe" required>
-<br />
-  <input type="submit" value="se connecter" name="ok">
-</form>
+    </form>
+  </div>
 </body>
+
 </html>
+
+
+
+
+<!-- <form method="POST" action="">
+
+  <label for="name">name</label>
+  <input type="name" id="name" name="name" placeholder="entrez votre nom" required>
+  <br />
+
+  <label for="password">mot de passe</label>
+  <input type="password" id="password" name="password" placeholder="entrez votre mot de passe" required>
+  <br />
+  <input type="submit" value="se connecter" name="ok">
+</form> -->
--- inscription.php
+++ inscription.php
@@ -4,10 +4,32 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Inscription</title>
+  <link rel="stylesheet" href="/style.css">
 </head>
 <body>
 
-<form method="POST" action="traitementInscription.php">
+<div class="block">
+        <h2>S'enregistrer</h2>
+        <form method="POST" action="traitementInscription.php">
+            <div class="inputs">
+                <input type="text" id="name" name="name" class="input" placeholder="entrez votre nom" required>
+                <input type="password" id="password" name="password"  class="input" placeholder="entrez votre mot de passe" required>
+                <input type="password" id="confirmPassword" class="input" placeholder="confirm password">
+            </div>
+            <input type="submit" value="M'inscrire" class="btn" name="ok" onclick="verify => {
+              if(document.getElementById('confirmPassword').value === document.getElementById('password').value){
+                console.log('test ok');
+              } else {
+                alert('les mdp ne correspondent pas!');
+              }
+            }">
+        </form>
+    </div>
+
+</body>
+</html>
+
+<!-- <form method="POST" action="traitementInscription.php">
 
 <label for="name">votre nom</label>
 <input type="text" id="name" name="name" placeholder="entrez votre nom" required>
@@ -18,7 +40,5 @@
 <br />
 
 <input type="submit" value="m'inscrire" name="ok">
-</form>
+</form> -->
   
-</body>
-</html>
