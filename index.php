c78aec8cb02355a6cb3a025d771585e41aad50d7
kilianrdgs <kilian.rdgs@free.fr>
2024-01-12 11:10:58 +0100

    ajout index et accueil

--- index.php
+++ index.php
@@ -4,31 +4,16 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Accueil</title>
+  <title>Bienvenue</title>
   <link rel="stylesheet" href="/style.css">
 </head>
 <body>
-<div class="block-2">
-        <div class="block-2-header">
-            <h3>Liste des utilisateurs (0)</h3>
-        </div>
-        <div class="list">
-            <ul>
-                <li>
-                    <div class="user">
-                        <h4>- Samba</h4>
-                        <div>
-                            <input type="button" class="btn" value="supprimer">
-                        </div>
-                    </div>
-                    <div class="user">
-                        <h4>- Samba</h4>
-                        <div>
-                            <input type="button" class="btn" value="supprimer">
-                        </div>
-                    </div>
-                </li>
-            </ul>
+
+<div class="block-3">
+        <h3>Gestionnaire de base de donnée</h3>
+        <div class="block-3-inputs">
+            <a href="connexion.php"><input type="button" class="btn" value="Connexion"></a>
+            <a href="insription.php"><input type="button" class="btn" value="Inscription"></a>
         </div>
     </div>
 
