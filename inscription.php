1a47e0fbc2f3b904f6b20e859c738c4219d4c8d6
Turbotard <turbotard9@gmail.com>
2024-01-12 11:03:51 +0100

    Refactor inscription form and database connection code

--- inscription.php
+++ inscription.php
@@ -1,37 +1,26 @@
 <!DOCTYPE html>
 <html lang="fr">
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Inscription</title>
-  <link rel="stylesheet" href="/style.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Inscription</title>
+    <link rel="stylesheet" href="/style.css">
 </head>
 <body>
 
 <div class="block">
-        <h2>S'enregistrer</h2>
-        <form method="POST" action="traitementInscription.php">
-            <div class="inputs">
-                <input type="text" id="name" name="name" class="input" placeholder="entrez votre nom" required>
-                <input type="password" id="password" name="password"  class="input" placeholder="entrez votre mot de passe" required>
-            </div>
-            <input type="submit" value="M'inscrire" class="btn" name="ok">
-        </form>
-    </div>
+    <h2>S'enregistrer</h2>
+    <form method="POST" action="traitementInscription.php">
+        <div class="inputs">
+            <label for="name">Votre nom :</label>
+            <input type="text" id="name" name="name" class="input" placeholder="Entrez votre nom" required>
+
+            <label for="password">Votre mot de passe :</label>
+            <input type="password" id="password" name="password" class="input" placeholder="Entrez votre mot de passe" required minlength="8">
+        </div>
+        <input type="submit" value="M'inscrire" class="btn" name="ok">
+    </form>
+</div>
 
 </body>
 </html>
-
-<!-- <form method="POST" action="traitementInscription.php">
-
-<label for="name">votre nom</label>
-<input type="text" id="name" name="name" placeholder="entrez votre nom" required>
-<br />
-
-<label for="password">votre mot de passe</label>
-<input type="password" id="password" name="password" placeholder="entrez votre mot de passe" required>
-<br />
-
-<input type="submit" value="m'inscrire" name="ok">
-</form> -->
-  
--- traitementInscription.php
+++ traitementInscription.php
@@ -7,23 +7,22 @@ $username = $env['username'];
 $password = $env['password'];
 
 try {
-$bdd = new PDO("mysql:host=$servername;dbname=users", $username, $password);
-$bdd -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-echo "connexion réussie";
-}
-catch (PDOException $e) {
-echo "connexion échouée: " . $e->getMessage();
+    $bdd = new PDO("mysql:host=$servername;dbname=users", $username, $password);
+    $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "connexion réussie";
+} catch (PDOException $e) {
+    echo "connexion échouée: " . $e->getMessage();
 }
 
 if (isset($_POST['ok'])) {
     $name = $_POST['name'];
     $password = $_POST['password'];
 
-    $requete = $bdd->prepare("INSERT INTO user VALUES (0, :name, :password)");
+    $requete = $bdd->prepare("INSERT INTO user (name, password) VALUES (:name, :password)");
     $requete->execute(array(
         "name" => $name,
         "password" => $password
     ));
     echo "inscription réussie";
-  }
+}
 ?>
