eca73b211cba4ac3cbe366a1426815389a8bb01f
Turbotard <turbotard9@gmail.com>
2024-01-12 13:37:21 +0100

    Add session start and redirect after successful user registration

--- traitementInscription.php
+++ traitementInscription.php
@@ -1,4 +1,5 @@
 <?php
+session_start(); // Démarrage de la session
 
 $env = parse_ini_file('variables.env');
 
@@ -7,33 +8,30 @@ $username = $env['username'];
 $password = $env['password'];
 
 try {
-  $bdd = new PDO("mysql:host=$servername;dbname=users", $username, $password);
-  $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  echo "connexion réussie";
+    $bdd = new PDO("mysql:host=$servername;dbname=users", $username, $password);
+    $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "connexion réussie";
 } catch (PDOException $e) {
-  echo "connexion échouée: " . $e->getMessage();
+    echo "connexion échouée: " . $e->getMessage();
 }
 
 if (isset($_POST['ok'])) {
-  echo "ok";
-  $name = $_POST['name'];
-  $password = $_POST['password'];
-  echo $name;
-  echo $password;
-  try {
-    $requete = $bdd->prepare("INSERT INTO user (name, password) VALUES (:name, :password)");
-    $requete->execute(
-      array(
-        "name" => $name,
-        "password" => $password
-      )
-    );
-    echo "inscription réussie";
-  } catch (PDOException $e) {
-    echo "Erreur lors de l'inscription: " . $e->getMessage();
-  }
+    $name = $_POST['name'];
+    $password = $_POST['password'];
 
+    try {
+        $requete = $bdd->prepare("INSERT INTO user (name, password) VALUES (:name, :password)");
+        $requete->execute(array("name" => $name, "password" => $password));
 
-  echo "inscription réussie";
+        $last_id = $bdd->lastInsertId();
+
+        $_SESSION['user_id'] = $last_id;
+        $_SESSION['user_name'] = $name;
+
+        header("Location: home.php");
+        exit;
+    } catch (PDOException $e) {
+        echo "Erreur lors de l'inscription: " . $e->getMessage();
+    }
 }
 ?>
