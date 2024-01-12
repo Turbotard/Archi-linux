d97fa1c53afb1342968badb2d662bfefe03b7bc6
Sambachpx <113524274+Sambachpx@users.noreply.github.com>
2024-01-12 11:00:22 +0100

    Fix password input formatting in inscription.php

--- inscription.php
+++ inscription.php
@@ -13,7 +13,7 @@
         <form method="POST" action="traitementInscription.php">
             <div class="inputs">
                 <input type="text" id="name" name="name" class="input" placeholder="entrez votre nom" required>
-                <input type="password" id="password" name="password"  class="input" placeholder="entrez votre mot de passe" required>
+                <input type="password" id="password" name="password" class="input" placeholder="entrez votre mot de passe" required>
                 <p>Vous avez déjà un compte ? <a href="connexion.php" style="text-decoration: none; color: blue;">connectez-vous !</a></p>
             </div>
             <input type="submit" value="m'inscrire" class="btn" name="ok">
